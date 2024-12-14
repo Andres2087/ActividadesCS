b5e47a456ac0537ab0ad68542279efe5fc294a30
Andres2087 <andres2078@gmail.com>
2024-12-14 12:35:51 -0500

    git action selenium

--- Actividad5/tests.py
+++ Actividad5/tests.py
@@ -8,33 +8,50 @@ import unittest
 import time
 from time import sleep
 
+# Configurar opciones para el navegador Chrome
+options = Options()
+options.add_argument('--headless') # Ejecuta Chrome en modo headless
+options.add_argument('--no-sandbox') # Recomendado en entornos de CI como GitHub Actions
+options.add_argument('--disable-dev-shm-usage') # Ayuda a evitar algunos errores en contenedores
+
 class AnimeFLVTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome(options=options)
 
     def test_list_animes_en_emision(self):
         browser = self.browser
         browser.maximize_window()
         browser.get("https://www3.animeflv.net/")  
-        self.assertIn("AnimeFLV", browser.title)
 
         contenedor = browser.find_element(By.ID, "mCSB_1")
         animes_list = contenedor.find_elements(By.TAG_NAME, "a")  
 
-        print("Animes en emisión:")
+        res = []
         for anime in animes_list:
-            print(anime.text)
+            res.append(anime.text)
+
+        self.assertGreater(len(res), 0, "No se encontraron animes en emisión.")
+
+        print("Animes en emisión:")
+        for anime in res:
+            print(anime)
 
     def test_busqueda_anime(self):
         browser = self.browser
         browser.maximize_window()
         browser.get("https://www3.animeflv.net/")  
-        self.assertIn("AnimeFLV", browser.title)
         search = browser.find_element(By.ID, "search-anime")
         search.clear()
         search.send_keys("Naruto")
         search.submit()
+        sleep(5) 
+        results = browser.find_elements(By.CLASS_NAME, "ListAnimes")  # Ajusta la clase si es diferente
+        self.assertGreater(len(results), 0, "No se encontraron resultados para 'Naruto'.")
+
+        
+
+       
         
 
     def tearDown(self):
